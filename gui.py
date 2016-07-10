7b3be76cc304c543b44786839655919f090de181
primaeval <primaeval@users.noreply.github.com>
2016-07-10 20:51:12 +0200

    tweak

--- gui.py
+++ gui.py
@@ -518,7 +518,7 @@ class TVGuide(xbmcgui.WindowXML):
         if ADDON.getSetting('program.background.enabled') == 'true' and program.imageSmall is not None:
             self.setControlImage(self.C_MAIN_BACKGROUND, program.imageSmall)
         else:
-            self.setControlImage(self.C_MAIN_BACKGROUND, "tvguide-program-grey.png")
+            self.setControlImage(self.C_MAIN_BACKGROUND, "black.png")
 
         if not self.osdEnabled and self.player.isPlaying():
             self.player.stop()
