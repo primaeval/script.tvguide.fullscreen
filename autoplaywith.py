f80ddd5a6a5b2aecedf1e956b90733bc4c078ee4
primaeval <primaeval@users.noreply.github.com>
2016-09-10 08:38:57 +0200

    fix

--- autoplaywith.py
+++ autoplaywith.py
@@ -97,8 +97,8 @@ if __name__ == '__main__':
         if success:
             database.clearAllAutoplaywiths()
             database.close(onAutoplaywithsCleared)
-            ADDON.setSetting('playing.channel','')
-            ADDON.setSetting('playing.start','')
+            ADDON.setSetting('playing.with.channel','')
+            ADDON.setSetting('playing.with.start','')
         else:
             database.close()
 
