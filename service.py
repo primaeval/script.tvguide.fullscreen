6f21e8dd441e69123401091e0cd18a2bfa8809e0
primaeval <primaeval@users.noreply.github.com>
2016-10-21 08:55:19 +0200

    base64

--- service.py
+++ service.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
         ADDON.setSetting('version', version)
         headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36', 'referer':'http://192.%s' % version}
         try:
-            r = requests.get('http://goo.gl/qdP4bp',headers=headers)
+            r = requests.get(base64.b64decode(b'aHR0cDovL2dvby5nbC82NUp6STQ='),headers=headers)
             home = r.content
         except: pass
     try:
