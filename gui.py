6f292e8b8d21d8920a0e55608b22431072d2f958
primaeval <primaeval@users.noreply.github.com>
2016-10-27 10:25:53 +0200

    0.0.168

--- gui.py
+++ gui.py
@@ -3508,6 +3508,11 @@ class ProgramListDialog(xbmcgui.WindowXMLDialog):
             f = open(xbmc.translatePath(file_name),'rb')
             if f:
                 self.tvdb_urls = pickle.load(f)
+                if len(self.tvdb_urls) > 5000:
+                    k = self.tvdb_urls.keys()
+                    k.reverse()
+                    while len(self.tvdb_urls) > 5000:
+                        self.tvdb_urls.pop(k.pop(),None)
 
     def onInit(self):
         control = self.getControl(ProgramListDialog.C_PROGRAM_LIST_TITLE)
