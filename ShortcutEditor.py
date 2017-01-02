103575bc90eb07f23c8f591f29e1f8c6883ae956
primaeval <primaeval@users.noreply.github.com>
2017-01-02 13:33:44 +0100

    initialise shortcut editor

--- ShortcutEditor.py
+++ ShortcutEditor.py
@@ -5,7 +5,9 @@ import xbmcaddon
 import xbmcvfs
 import json
 
-file_name = 'special://profile/addon_data/script.tvguide.fullscreen/channel_id_shortcut.ini'
+id_shortcuts = {}
+
+file_name = 'special://profile/addon_data/script.tvguide.fullscreen/channel_id_title.ini'
 f = xbmcvfs.File(file_name,'rb')
 data = f.read()
 f.close()
@@ -17,13 +19,28 @@ if data:
         if len(id_shortcut) == 2:
             id_shortcuts[id_shortcut[0]] = id_shortcut[1]
 
+shortcut_file_name = 'special://profile/addon_data/script.tvguide.fullscreen/channel_id_shortcut.ini'
+f = xbmcvfs.File(shortcut_file_name,'rb')
+data = f.read()
+f.close()
+
+if data:
+    lines = data.splitlines()
+    for line in lines:
+        id_shortcut = line.split("=")
+        if len(id_shortcut) == 2:
+            shortcut = id_shortcut[1]
+            if shortcut in id_shortcuts:
+                del id_shortcuts[shortcut]
+            id_shortcuts[id_shortcut[0]] = id_shortcut[1]
+
 d = xbmcgui.Dialog()
 while True:
     ids = sorted(id_shortcuts.keys())
     labels = ["%s - %s" % (x, id_shortcuts[x]) for x in ids]
     which = d.select("Shortcut Editor (id - numeric shortcut)",labels)
     if which == -1:
-        f = xbmcvfs.File(file_name,'wb')
+        f = xbmcvfs.File(shortcut_file_name,'wb')
         for id in sorted(id_shortcuts):
             f.write("%s=%s\n" % (id.encode("utf8"),id_shortcuts[id].encode("utf8")))
         f.close()
