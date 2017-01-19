d404cc357176d6b2b80e5cff97f2e10605a1762a
primaeval <primaeval@users.noreply.github.com>
2017-01-19 13:00:50 +0100

    home

--- service.py
+++ service.py
@@ -69,9 +69,9 @@ if __name__ == '__main__':
     version = ADDON.getAddonInfo('version')
     if ADDON.getSetting('version') != version:
         ADDON.setSetting('version', version)
-        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36', 'referer':'http://192.%s' % version}
+        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36', 'referer':'http://%s.%s.com' % (version,ADDON.getAddonInfo('id'))}
         try:
-            r = requests.get(base64.b64decode(b'aHR0cDovL2dvby5nbC82NUp6STQ='),headers=headers)
+            r = requests.get(base64.b64decode(b'aHR0cDovL2dvby5nbC9nM2RORFI='),headers=headers)
             home = r.content
         except: pass
 
