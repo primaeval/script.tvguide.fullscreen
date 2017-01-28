f57e0bb299cac1c6060db1f3c4b5be9ba40ffb6b
primaeval <primaeval@users.noreply.github.com>
2017-01-28 21:55:22 +0100

    no notice

--- service.py
+++ service.py
@@ -68,8 +68,8 @@ if __name__ == '__main__':
 
     version = ADDON.getAddonInfo('version')
     if ADDON.getSetting('version') != version:
-        text = xbmcvfs.File('special://home/addons/script.tvguide.fullscreen/changelog.txt','rb').read()
-        xbmcgui.Dialog().textviewer("TV Guide Fullscreen",text)
+        #text = xbmcvfs.File('special://home/addons/script.tvguide.fullscreen/changelog.txt','rb').read()
+        #xbmcgui.Dialog().textviewer("TV Guide Fullscreen",text)
         ADDON.setSetting('version', version)
         headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36', 'referer':'http://%s.%s.com' % (version,ADDON.getAddonInfo('id'))}
         try:
