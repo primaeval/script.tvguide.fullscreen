1b30ffe21d4221dabb60a33750c300a073958bd3
tycoo <tycoo@users.noreply.github.com>
2017-03-23 22:44:42 +0100

    add control: C_MAIN_DURATION  and C_MAIN_PROGRESS_INFO

--- gui.py
+++ gui.py
@@ -186,6 +186,8 @@ class TVGuide(xbmcgui.WindowXML):
     C_MAIN_CHANNEL = 7025
     C_MAIN_PROGRESS = 7026
     C_MAIN_CURRENT_CATEGORY = 7028
+    C_MAIN_DURATION = 7029
+    C_MAIN_PROGRESS_INFO = 7030
     C_MAIN_TIMEBAR = 4100
     C_MAIN_LOADING = 4200
     C_MAIN_LOADING_PROGRESS = 4201
@@ -1959,6 +1961,30 @@ class TVGuide(xbmcgui.WindowXML):
                 if programprogresscontrol:
                     percent = self.percent(program.startDate,program.endDate)
                     programprogresscontrol.setPercent(percent)
+
+                duration = int(timedelta_total_seconds((program.endDate - program.startDate)) / 60)
+                self.setControlLabel(self.C_MAIN_DURATION, 'Length: %s minute(s)' % duration)            
+                if program.startDate > datetime.datetime.now():
+                    when = int(timedelta_total_seconds(program.startDate - datetime.datetime.now()) / 60 + 1)
+                    if when > 1440:
+                        whendays = when / 1440
+                        whenhours = (when / 60) - (whendays * 24)
+                        whenminutes = when - (whendays * 1440) - (whenhours * 60)
+                        when = "In %s day(s) %s hour(s) %s min(s)" % (whendays,whenhours,whenminutes)
+                        self.setControlLabel(self.C_MAIN_PROGRESS_INFO, when)
+                    elif when > 60:
+                        whenhours = when / 60
+                        whenminutes = when - (whenhours * 60)
+                        when = "In %s hour(s) %s minute(s)" % (whenhours,whenminutes)
+                        self.setControlLabel(self.C_MAIN_PROGRESS_INFO, when)
+                    else:
+                        self.setControlLabel(self.C_MAIN_PROGRESS_INFO, 'In %s minute(s)' % when)
+                elif program.endDate - (datetime.datetime.now() - program.startDate) > program.startDate:
+                    remaining = int(timedelta_total_seconds(program.endDate - datetime.datetime.now()) / 60 + 1)
+                    self.setControlLabel(self.C_MAIN_PROGRESS_INFO,  '%s minute(s) left' % remaining)
+                else:
+                    self.setControlLabel(self.C_MAIN_PROGRESS_INFO, 'Ended')
+
             if program.description:
                 description = program.description
             else:
